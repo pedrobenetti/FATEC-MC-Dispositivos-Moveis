8d73f69f661a7f9881ab0ef7ae6e4c09097eaf1a
pedrobenetti <pedrobenetti@outlook.com.br>
2022-03-17 18:22:33 -0300

    Serviço  para criação de usuario

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+interface IUserRequest{        //
+    name: string;              // Interface para definir o tipo dos dados que serão requistados do usuário.
+    email: string;             // Isso impede que o user forneça uma palavra no campo de telefone, por exemplo.
+    admin?: boolean;           //
+    password: string;          
+}
+
+class CreateUserService{
+    async execute({ name, email, admin = false, password}: IUserRequest) {
+        
+        if(!email){
+            throw new Error("Email Incorrect!");
+        }
+
+        let vuser = {
+            name: "Pedro", email: "pedrobenetti@outlook.com.br", admin: false, password: 1234
+        }
+
+        return vuser;
+    }
+/** 
+ *
+Método assíncrono chamado excute. Na primeira linha, recebe os dados listados na interface.
+O IF serve para configurar o email sempre como incorreto, apenas para testes. Logo abaixo, um objeto para simular a entrada de 
+valores das variáveis. 
+
+O principal emprego dessa função será validar dados para evitar problemas como duplicidade de email, nomes 
+repetidos, password com caracteres insuficientes, etc.
+
+Toda chamada de criação de usuário deverá passar por essa autenticação antes de ir para a base de dados.
+*
+*/
+}
+export { CreateUserService };
