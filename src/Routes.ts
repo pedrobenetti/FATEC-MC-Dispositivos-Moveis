2c5db79dfd9291128c5e6833a35e059b7f842d3f
pedrobenetti <pedrobenetti@outlook.com.br>
2022-03-27 14:40:25 -0300

    Criação de rotas para autenticação de user e admin

--- src/Routes.ts
+++ src/Routes.ts
@@ -3,25 +3,29 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { ListUserController } from "./controllers/ListUserController";
 import { DeleteUserController } from "./controllers/DeleteUserController";
 import { UpdateUserController } from "./controllers/UpdateUserController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+
+import { ensureAdmin } from "./middlewares/EnsureAdmin";
+import { ensureAuthenticated } from "./middlewares/EnsureAuthenticated";
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
 const deleteUserController = new DeleteUserController();
 const updateUserController = new UpdateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 
 const router = Router();
 
 router.post("/users", createUserController.handle);
-router.get("/users", listUserController.handle);
-router.delete("/users/:id", deleteUserController.handle);
+router.post("/login", authenticateUserController.handle);
+
+router.use(ensureAuthenticated);
 router.put("/users", updateUserController.handle);
+
+router.use(ensureAdmin);
+router.delete("/users/:id", deleteUserController.handle);
+router.get("/users", listUserController.handle);
 
 
 export { router };
-
-
-
-/**
- * Arquivo principal para configuração de rotas.
- */
