c5a020d0991d21d32f607013efc5378b139b3ead
pedrobenetti <pedrobenetti@outlook.com.br>
2022-04-06 01:46:00 -0300

    Implementação da exclusão de User

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { compare } from "bcryptjs";
 import { hash } from "bcryptjs";
 import { sign } from "jsonwebtoken";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 interface IAuthenticateRequest {
   email: string;
@@ -10,29 +12,43 @@ interface IAuthenticateRequest {
 class AuthenticateUserService {
   async execute({ email, password }: IAuthenticateRequest) {
 
-    if(email!="prog@fatec.sp.gov.br") {
-        throw new Error("Email incorrect");
+    if(!email){
+      throw new Error('Email required!');
     }
 
-    const passwordHash = await hash("fatec", 8);
-    // 123456 / 783645734-sdhfhsdf7762374234234    
-    const passwordMatch = await compare(password, passwordHash);
+  if(!password){
+      throw new Error('Password required!');
+    } 
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+        email,
+    });
+
+    if(!user){
+        throw new Error ("Email not found!");
+
+    }
+
+    //const passwordHash = await hash(password, 8);
+    const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
       throw new Error("Password incorrect");
     }
 
-    // Gerar token    
     const token = sign(
       {
-        email:"prog@fatec.sp.gov.br",
+        email: user.email,
       },
       "4f93ac9d10cb751b8c9c646bc9dbccb9",
       {
-        subject: "Admin",
+        subject: user.id,
         expiresIn: "1d",
       }
     );
+
     return token;
   }
 }
