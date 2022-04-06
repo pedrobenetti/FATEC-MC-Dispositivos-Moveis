7e234f71fafb63d612954cf2d2b191c7dfc1201e
pedrobenetti <pedrobenetti@outlook.com.br>
2022-04-06 02:20:47 -0300

    Remoção do atributo email

--- src/controllers/UpdateUserController.ts
+++ src/controllers/UpdateUserController.ts
@@ -3,14 +3,13 @@ import { UpdateUserService } from "../services/UpdateUserService";
 
 class UpdateUserController{
     async handle(request: Request, response: Response){
-        const{id, name, email, admin, password} = request.body;
+        const{id, name, admin, password} = request.body;
 
         const updateUserService = new UpdateUserService();
         
         const user = await updateUserService.execute({
             id,
             name, 
-            email,
             admin, 
             password
         });
