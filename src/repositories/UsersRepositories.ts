5f0a156d90cdd5cfee1c58c15953077719f84cef
pedrobenetti <pedrobenetti@outlook.com.br>
2022-04-05 23:07:09 -0300

    Criação do repositório de user

---
+++ src/repositories/UsersRepositories.ts
@@ -0,0 +1,7 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+
+@EntityRepository(User)
+class UserRepositories extends Repository<User> {};
+
+export { UserRepositories };
